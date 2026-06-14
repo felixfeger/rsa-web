fab0e9f6aaee8a8e35a3246c6e3ba98445dda329
felixfeger <felixfeger46@gmail.com>
2026-06-13 19:23:58 -0700

    Modify emergency incident details and timestamps
    Updated emergency data with new last updated timestamp and modified incident details.

--- emergency-data.js
+++ emergency-data.js
@@ -1,16 +1,16 @@
 window.rsaEmergencyData = {
   noCurrentEmergencies: false,
-  lastUpdated: "2026-06-12 14:00 PT",
+  lastUpdated: "2026-06-13 19:25 PT",
   message: "One emergency incident.",
   emergencies: [
     {
       title: "Heartlake City Bridge Collaspe",
-      status: "On Scene",
-      severity: "Critical",
+      status: "Under Control",
+      severity: "Severe",
       area: "Heartlake City",
-      updated: "2026-06-12 14:00 PT",
-      summary: "The bridge over Interstate 5 has collapsed. Interstate 5 is closed at this time. There is signifigant damage to the surrounding city. ",
-      instructions: "Republic Highway Patrol and Repubic States Federal Police have begun evacuations. All residents are urged to evacuate either to outside the city limits or to Lego City. City Metro is currently providing free bus shuttles to Lego City. Listen to emergency staff for directions."
+      updated: "2026-06-13 19:25 PT",
+      summary: "The bridge over Interstate 5 has collapsed. Interstate 5 has substained minimal damage. There is signifigant damage to the surrounding city. ",
+      instructions: "Republic Highway Patrol and Repubic States Federal Police executed evacuations that have since ended as of June 13th. All residents are allowed to return to their homes and other properties. Access is currently restricted to residents only. Interstate 5 has partially reopened. "
     }
   ]
 };
