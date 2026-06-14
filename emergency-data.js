04485b300ffd4973db1120e129a192173f9b5e02
felixfeger <felixfeger46@gmail.com>
2026-06-14 05:39:32 -0700

    Update emergency-data.js

--- emergency-data.js
+++ emergency-data.js
@@ -1,16 +1,16 @@
 window.rsaEmergencyData = {
   noCurrentEmergencies: false,
-  lastUpdated: "2026-06-13 19:25 PT",
+  lastUpdated: "2026-06-14 5:39 PDT",
   message: "One emergency incident.",
   emergencies: [
     {
       title: "Heartlake City Bridge Collaspe",
-      status: "Under Control",
+      status: "Resolved",
       severity: "Severe",
       area: "Heartlake City",
-      updated: "2026-06-13 19:25 PT",
+      updated: "2026-06-14 5:39 PDT",
       summary: "The bridge over Interstate 5 has collapsed. Interstate 5 has substained minimal damage. There is signifigant damage to the surrounding city. ",
-      instructions: "Republic Highway Patrol and Repubic States Federal Police executed evacuations that have since ended as of June 13th. All residents are allowed to return to their homes and other properties. Access is currently restricted to residents only. Interstate 5 has partially reopened. "
+      instructions: "Republic States Emergency Management has directed that the Republic States Fire Dept. and RHP should be removed from the scene, and Heartlake City Rescue will be incharge of recovery efforts. At this time all RHP, RFD, and federal aid have left Heartlake City. At this time access is open to everyone. A portion of Heartlake City remains closed, but Interstate 5 has fully reopened. For questions or concerns please contact Heartlake City Resuce."
     }
   ]
 };
