efa062000eeadee9ecc8a57d54869bfcf0b48ea0
felixfeger <felixfeger46@gmail.com>
2026-09-23 10:18:20 -0700

    Update emergency-data.js

--- emergency-data.js
+++ emergency-data.js
@@ -1,16 +1,16 @@
 window.rsaEmergencyData = {
-  noCurrentEmergencies: true,
+  noCurrentEmergencies: false,
   lastUpdated: "2026-06-18 17:17 PDT",
   message: "There are no active Republic States Emergency Management incidents. Please check with local, county, and state officials for updates from the local, county, and state incidents.",
   emergencies: [
     {
-      title: "Heartlake City Bridge Collaspe",
-      status: "Resolved",
-      severity: "Severe",
-      area: "Heartlake City",
-      updated: "2026-06-14 5:39 PDT",
-      summary: "The bridge over Interstate 5 has collapsed. Interstate 5 has substained minimal damage. There is signifigant damage to the surrounding city. ",
-      instructions: "Republic States Emergency Management has directed that the Republic States Fire Dept. and RHP should be removed from the scene, and Heartlake City Rescue will be incharge of recovery efforts. At this time all RHP, RFD, and federal aid have left Heartlake City. At this time access is open to everyone. A portion of Heartlake City remains closed, but Interstate 5 has fully reopened. For questions or concerns please contact Heartlake City Resuce."
+      title: "Possibile Outbreak of Flu Virus in Lego City",
+      status: "Identified",
+      severity: "Minor",
+      area: "Lego City",
+      updated: "2026-09-23 10:15 PDT",
+      summary: "There are several confirmed cases of Flu in Lego City.",
+      instructions: "Republic States Department of Public Health has suggested that individuals begin using masks and social distance. City Metro has implememted specific protocals on their trains and buses.   For questions or concerns please contact Republic States Department of Public Health or City Metro."
     }
   ]
 };
